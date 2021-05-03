631cceb1f0023b91e50499323d8d7148dbb10526
abc <joel.rajesh13@gmail.com>
2021-05-03 10:37:14 +0530

    Infinite Scroll Assignment refactor

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,24 +1,25 @@
 let quotesDiv = document.querySelector(".quotes");
+let index = 0
 
 
-
- function loadMore(){
+ function addMore(){
     for(let i=0 ; i< 6; i++) {
        let div =  document.createElement("div");
        div.classList.add("quote");
        let h3 = document.createElement("h3");
        let p = document.createElement("p");
-       h3.innerText = quotes[i].quoteText;
-       p.innerText = quotes[i].quoteAuthor;
+       h3.innerText = quotes[index].quoteText;
+       p.innerText = quotes[index].quoteAuthor;
        div.append(h3,p)
        quotesDiv.append(div);
+       index++
     }
 }
 
-loadMore()
+addMore()
 
 quotesDiv.addEventListener('scroll', function() {
     if (quotesDiv.scrollTop + quotesDiv.clientHeight >= quotesDiv.scrollHeight) {
-      loadMore();
+      addMore();
     }
   });
